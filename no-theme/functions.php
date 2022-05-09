61a9e7d63d092747e746ca59267d202a8c5177e3
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2022-05-09 22:23:25 +0700

    added releases

--- no-theme/functions.php
+++ no-theme/functions.php
@@ -9,4 +9,9 @@
     }
     add_action( 'init', 'register_my_menus' );
     add_theme_support('post-thumbnails');
+
+    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    activate_plugin( 'wp-graphql/wp-graphql.php' );
+
+    switch_theme('no-theme');
 ?>
